11634f0cc64c2c2ee30d6d2ab1d74c8348d3a8a0
varnita619 <crabthe64@gmail.com>
2023-04-04 02:28:20 +0530

    update: url

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -30,7 +30,8 @@ const Auth = () => {
     const { username, password, phoneNumber, avatarURL } = form;
 
     // const URL = "http://localhost:5000/auth";
-    const URL = "https://medico-helper.onrender.com/";
+    // const URL = 'https://chatbes.herokuapp.com/auth';
+    const URL = "https://medico-helper.onrender.com/auth";
 
     const {
       data: { token, userId, hashedPassword, fullName },
